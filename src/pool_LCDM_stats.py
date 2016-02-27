efb3e6f993fb1e3c4f91562a6b65b0c570d70b41
Sancar Adali <sadali1@jhu.edu>
2016-02-27 01:46:29 -0500

    fix syntax errors of pool python script

--- src/pool_LCDM_stats.py
+++ src/pool_LCDM_stats.py
@@ -1,10 +1,10 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 __author__ = 'Sancar'
 import os
-import os.path.join as join_p
+from  os.path import join as join_p
 import glob
 
-
+ROOT = "/cis/home/sadali/MAS_score_analysis/"
 
 
 regions= ['cingulate','TL']
@@ -32,19 +32,23 @@ diags=[
 hemis=['rh','lh']
 
 wave1_dir1 = '/cis/project/sydney/data_fslcdm/MAS.20100921_Antsy/'
-wave1_dir2 = "/cis/project/sydney/data_fslcdm/MAS.20110324_Antsy/'
+wave1_dir2 = '/cis/project/sydney/data_fslcdm/MAS.20110324_Antsy/'
 for region in regions:
-    region_name = hemi+'_'+region
+
     for hemi in hemis:
+        region_name = hemi+'_'+region
         for diag in diags:
-            idfile='ids_5_subclass/Diag_'+diag+'_ID_list.csv' #wave1_${diag}_used.txt'
+            idfile=join_p(ROOT,'data','ids_5_subclass','Diag_'+diag+'_ID_list.csv' ) #wave1_${diag}_used.txt'
             id_fh = open(idfile,'r')
             ids = id_fh.readlines()
             for id in ids:
 
                 for sub in subregions[region]:
                     sub_region=hemi+'_'+sub
-                    output_fname = '/cis/home/sadali/pooled_analysis/pooled_5subclass_antsy_files_used/'+sub_region+'_'+diag+'_wave1_pooled_antsy.txt'
+                    output_dir = join_p(ROOT,'data','pooled_analysis','pooled_5subclass_antsy_files_used')
+                    if not os.path.exists(output_dir):
+                        os.makedirs(output_dir)
+                    output_fname = join_p(output_dir,sub_region+'_'+diag+'_wave1_pooled_antsy.txt')
                     #of_handle = open (output_fname,'a')
 
                     for id in ids:
@@ -52,7 +56,7 @@ for region in regions:
                     #   full_dir_names = os.listdir(wave1_dir1)
                     #   full_path =  os.path.join(wave1_dir1, x for x in full_dir_names if x.startswith(id), 'FSLCDM_v2.3',region_name,sub_region+'_1_manseg_quartile_pialmsk_AntsyGrey.txt')
                         full_path =  os.path.join(wave1_dir1, id+"*", 'FSLCDM_v2.3',region_name,sub_region+'_1_manseg_quartile_pialmsk_AntsyGrey.txt')
-                        f = glob.glob(full_path):
+                        f = glob.glob(full_path)
 
                         if os.path.exists(f) and os.isfile(f) and os.path.getsize(f)>0:
                             os.system("cat "+f+" >> "+output_fname)
